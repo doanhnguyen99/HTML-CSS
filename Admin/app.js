212ab378a834bdc580633b5507f7019dbcda87ce
Doanh Nguyễn <doanhnh@talent.ominext.com>
2020-01-17 09:10:31 +0700

    Admin UI Full

--- Admin/app.js
+++ Admin/app.js
@@ -2,3 +2,20 @@
 //     $.datetimepicker.setLocale('pt-BR');
 //    $('#datetimepicker').datetimepicker();
 // });
+
+function myFunction()
+{
+    $.confirm({
+        title: 'Xóa!',
+        content: 'Bạn có chắc chắn không?',
+        buttons: {
+            Yes: function () {
+                $.alert('OK!');
+            },
+            No: function () {
+                
+            }
+            
+        }
+    });
+}
